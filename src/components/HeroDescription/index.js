17a1ee7df55b7d5f1b3be669394e682a1a28b86d
Caio Macbook <capoyares@gmail.com>
2019-09-09 22:04:52 -0300

    added HeroDescription component

--- src/components/HeroDescription/index.js
+++ src/components/HeroDescription/index.js
@@ -6,34 +6,70 @@ import { CharactersContainer } from "./style";
 const apiKey = "68656f31c3623d9a8cfcc697750b60bc";
 
 const HeroDescription = ({ match }) => {
+  const [heroInfo, setHeroInfo] = useState({});
 
-    const [heroInfo, setHeroInfo] = useState({});
-    
-    useEffect(() => {
-        const heroId = match.params.heroId;
-        const endpoint = `https://gateway.marvel.com:443/v1/public/characters?id=${heroId}&apikey=${apiKey}`;
-        axios(endpoint)
-            .then(response => setHeroInfo(response.data.data.results[0]) || console.log(response.data.data.results[0]))
-    }, []);
+  useEffect(() => {
+    const heroId = match.params.heroId;
+    const endpoint = `https://gateway.marvel.com:443/v1/public/characters?id=${heroId}&apikey=${apiKey}`;
+    axios(endpoint).then(
+      response =>
+        setHeroInfo(response.data.data.results[0]) ||
+        console.log(response.data.data.results[0])
+    );
+  }, []);
 
-    const { thumbnail } = heroInfo;
+  const {
+    thumbnail,
+    description,
+    name,
+    series,
+    stories,
+    comics,
+    urls
+  } = heroInfo;
 
-    return (
-        <CharactersContainer>
-            {thumbnail && 
-                <div style={{display: "flex"}}>
-                    <div className="img-container" style={{flexBasis: "50vw"}}>
-                    {thumbnail && <img src={`${thumbnail.path}.${thumbnail.extension}`} />}
-                    </div>
-                    <div className="hero-description">
-
-                    </div>
-                </div>
-            }
-            
-        </CharactersContainer>
-        
-    )
-}
+  return (
+    <CharactersContainer>
+      {thumbnail && (
+        <>
+          <div className="img-container">
+            {thumbnail && (
+              <img src={`${thumbnail.path}.${thumbnail.extension}`} />
+            )}
+          </div>
+          <div>
+            <div className="hero-description">
+              <h3 className="hero__name">{name}</h3>
+              {description !== "" && (
+                <p className="hero__subtitle">{description}</p>
+              )}
+              <p>
+                <span>Quadrinhos: </span>
+                {comics.available}
+              </p>
+              <p>
+                <span>Séries: </span>
+                {series.available}
+              </p>
+              <p>
+                <span>Histórias: </span>
+                {stories.available}
+              </p>
+              <p>
+                <span>Quadrinhos: </span>
+                {comics.available}
+              </p>
+            </div>
+            {urls.length > 0 && (
+              <a href={urls[0].url} className="detail-btn" target="_blank">
+                Mais detalhes
+              </a>
+            )}
+          </div>
+        </>
+      )}
+    </CharactersContainer>
+  );
+};
 
 export default HeroDescription;
--- src/components/HeroDescription/style.js
+++ src/components/HeroDescription/style.js
@@ -4,6 +4,63 @@ const CharactersContainer = styled.div`
   max-width: 1100px;
   margin: 0 auto;
   text-align: center;
+  @media (min-width: 700px) {
+    text-align: left;
+  }
+  @media (min-width: 700px) {
+    display: flex;
+    margin: 0 auto;
+    padding: 0 1rem;
+    justify-content: center;
+  }
+
+  .img-container {
+    max-width: 100%;
+    overflow: hidden;
+    @media (min-width: 700px) {
+      min-width: 400px;
+    }
+    img {
+      display: block;
+      max-width: 300px;
+      margin: 0 auto;
+      @media (min-width: 700px) {
+        max-width: 400px;
+      }
+    }
+  }
+
+  .hero-description {
+    margin: 0 1.5rem;
+    .hero__subtitle {
+      margin: 0 auto;
+      max-width: 450px;
+      @media (min-width: 700px) {
+        max-width: none;
+      }
+    }
+    p {
+      color: #999;
+    }
+    span {
+      color: #333;
+    }
+  }
+
+  .detail-btn {
+    display: inline-block;
+    padding: 0.7rem 2rem;
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+    color: white;
+    background-color: #ed1d24;
+    border-radius: 4px;
+    text-decoration: none;
+    border: 2px solid crimson;
+    @media (min-width: 700px) {
+      margin-left: 1.5rem;
+    }
+  }
 `;
 
 export { CharactersContainer };
